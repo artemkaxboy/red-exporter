1b80a58cb16983e830ec80a57142af656bf82aa7
Artem Kolin <artemkaxboy@gmail.com>
2021-09-20 18:54:52 +0700

    Adds mysql and JPA dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.21"
 	kotlin("plugin.spring") version "1.5.21"
+	kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "com.artemkaxboy"
@@ -16,9 +17,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
