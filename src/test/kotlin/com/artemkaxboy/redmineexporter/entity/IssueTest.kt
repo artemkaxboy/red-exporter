8639bb7ad2f9b8b63ab864cb1f1dc9b67ad44765
Artem Kolin <artemkaxboy@gmail.com>
2021-10-07 00:43:55 +0700

    Adds some tests

--- src/test/kotlin/com/artemkaxboy/redmineexporter/entity/IssueTest.kt
+++ src/test/kotlin/com/artemkaxboy/redmineexporter/entity/IssueTest.kt
@@ -2,6 +2,9 @@ package com.artemkaxboy.redmineexporter.entity
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+import org.springframework.core.type.filter.AnnotationTypeFilter
+import javax.persistence.Entity
 import kotlin.reflect.full.memberProperties
 
 internal class IssueTest {
@@ -24,5 +27,22 @@ internal class IssueTest {
     fun testHashCode() {
 
         Assertions.assertThat(makeIssue().hashCode()).isEqualTo(0)
+    }
+
+    @Test //https://stackoverflow.com/questions/259140/scanning-java-annotations-at-runtime
+    fun allAnnotated() {
+        val scanner = ClassPathScanningCandidateComponentProvider(true)
+//        ClassPathScanningCandidateComponentProvider scanner =
+//        new ClassPathScanningCandidateComponentProvider(<DO_YOU_WANT_TO_USE_DEFALT_FILTER>);
+
+        scanner.addIncludeFilter(AnnotationTypeFilter(Entity::class.java))
+//        scanner.addIncludeFilter(new AnnotationTypeFilter(<TYPE_YOUR_ANNOTATION_HERE>.class));
+
+        scanner.findCandidateComponents("com.artemkaxboy").forEach {
+            println(it)
+        }
+//        for (BeanDefinition bd : scanner.findCandidateComponents(<TYPE_YOUR_BASE_PACKAGE_HERE>))
+//        System.out.println(bd.getBeanClassName());
+
     }
 }
